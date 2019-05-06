3fa44f4a30e2d5792eeb7727ef34039342ae5233
Anthony Crane <anthonycrane@hotmail.com>
2019-05-06 11:38:58 -0700

    More UI and functionality
    This work got broken up between yesterday and this morning.

---
+++ Dev-smack/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Dev-smack
+//
+//  Created by Mac User on 5/5/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
