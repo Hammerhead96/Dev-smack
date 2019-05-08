a4dd0701b4ae42796a6c2131a91dc76888c52cc4
Anthony Crane <anthonycrane@hotmail.com>
2019-05-07 18:58:36 -0700

    CreateAccountVC "finished"
    The CreateAccountVC has all its basic functionality completed. Notification Center logic is started.

---
+++ Dev-smack/View/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Dev-smack
+//
+//  Created by Mac User on 5/7/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+    override func awakeFromNib() {
+        setupView()
+    }
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
