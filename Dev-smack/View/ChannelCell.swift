117904a151c1a5441d5b02c8048a184c19b97b63
Anthony Crane <anthonycrane@hotmail.com>
2019-05-08 18:44:38 -0700

    "channels"
    more work on the imaginary channels

---
+++ Dev-smack/View/ChannelCell.swift
@@ -0,0 +1,32 @@
+//
+//  ChannelCell.swift
+//  Dev-smack
+//
+//  Created by Mac User on 5/8/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    func configureCell(channel: Channel) {
+        let title = channel.name ?? "" // .name == .channelTitle
+        channelName.text = "#\(title)"
+    }
+}
